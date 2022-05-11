cd484ec1623e48aa65a6b1415a276be979507f89
Gabriel <gabrielmora2001@hotmail.com>
2022-05-10 21:32:36 -0500

    register WIP

---
+++ src/utils/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+     apiKey: "AIzaSyCZ8xZdD4OnnD3oOY8Wm0jEmtyW4dUHnnI",
+  authDomain: "tallershop-web.firebaseapp.com",
+  projectId: "tallershop-web",
+  storageBucket: "tallershop-web.appspot.com",
+  messagingSenderId: "452610476628",
+  appId: "1:452610476628:web:13ad7b2d935460c9da7bc1"
+};
+
+export default firebaseConfig;
