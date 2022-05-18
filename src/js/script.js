aa8b57d75d3dd302b2345d8459de5396288a2613
Gabriel <gabrielmora2001@hotmail.com>
2022-05-18 00:25:37 -0500

    create product added

--- src/js/script.js
+++ src/js/script.js
@@ -1,58 +1,72 @@
-const user = {
-name: "juanito",
-mail: "juanito@gmail.com",
-age: "21"
-}
-console.log(user);
+import { auth, db } from "./functions/app";
+import { onAuthStateChanged } from "./functions/auth";
+import { getFirebaseCart, createFirebaseCart } from "./functions/cart";
+import { addProductToCart } from "../utils";
+import { getMyLocalCart, currencyFormat } from "../utils/index";
+const cartSection = document.getElementById("cart");
+const totalSection = document.getElementById("total");
+let cart = [];
 
-const products =[
-   {
-        name: "Boleta 3 días",
-        price: 880000,
-        id: 0,
-        image : "./img/boleta3.png",
-        quant:10,
-        description:"Una manilla RFID válida para ingresar los 3 días al Festival Estéreo Picnic"
-    },
-    {
-        name: "Boleta individual viernes",
-        price: 435000,
-        id: 1,
-        image : "./img/boletaV.png",
-        quant:10,
-        description:"Un ticket válido para ingresar el día viernes 25 de marzo al Festival Estéreo Picnic"
-    },
-    {
-        name: "Boleta individual sábado",
-        price: 435000,
-        id: 2,
-        image : "./img/boletaS.png",
-        quant:15,
-        description:"Un ticket válido para ingresar el día sábado 26 de marzo al Festival Estéreo Picnic"
-    },
-    {
-        name: "Boleta individual domingo",
-        price: 435000,
-        id: 3,
-        image : "./img/boletaD.png",
-        quant:7,
-        description:"Un ticket válido para ingresar el día domingo 27 de marzo al Festival Estéreo Picnic"
-    },
-    {
-        name: "Boleta 3 días VIP",
-        price: 1690000,
-        id: 4,
-        image : "./img/boleta3VIP.png",
-        quant:10,
-        description:"Una manilla RFID válida para ingresar los 3 días al Festival Estéreo Picnic, e incluye beneficios VIP"
-    },
-];
-products.forEach(e =>{
-    const {name,price,id,image,quant,description} = e
-    if(price>500000){
-        document.getElementById(`obj${id}`).style.display = "block";
-        document.getElementById(`obj${id}`).getElementsByClassName('name')[0].innerHTML = name;
-        document.getElementById(`obj${id}`).getElementsByClassName('image')[0].src = image;
+function loadCart(cart) {
+    let total = 0;
+    cart.forEach(product => {
+        renderProduct(product);
+        total += parseInt(product.price);
+    });
+
+    totalSection.innerText = currencyFormat(total);
+};
+
+async function removeProduct(productId) {
+    const newCart = cart.filter(product => product.id !== productId);
+    
+    cart = newCart;
+
+    if (userLogged) {
+        await createFirebaseCart(db, userLogged.uid, newCart);
     }
+
+    addProductToCart(newCart);
+
+    cartSection.innerHTML = "";
+
+    loadCart(newCart);
+
 }
-    );
+
+
+function renderProduct(product) {
+    const productCart = document.createElement("li");
+    productCart.className = "product";
+    productCart.innerHTML = `
+    <img src="${product.images[0]}" class="product__image">
+    <h2 class="product__name">${product.name}</h2>
+    <h3 class="product__price">${currencyFormat(product.price)}</h3>
+    <button class="product__delete">Eliminar producto</button>
+    `;
+
+    cartSection.appendChild(productCart);
+
+    productCart.addEventListener("click", e => {
+         if (e.target.tagName === "BUTTON") {
+             console.log("remove it!");
+             removeProduct(product.id);
+         }
+    })
+};
+
+onAuthStateChanged(auth, async (user) => {
+    if (user) {
+      // User is signed in, see docs for a list of available properties
+      // https://firebase.google.com/docs/reference/js/firebase.User
+      userLogged = user;
+      cart = await getFirebaseCart(db, userLogged.uid);
+    } else {
+        cart = getMyLocalCart();
+      // User is signed out
+      // ...
+    }
+
+    loadCart(cart);
+
+  });
---
+++ src/js/shop.js
@@ -0,0 +1,120 @@
+import { auth, db } from "./functions/app";
+import { onAuthStateChanged } from "./functions/auth";
+import { getProducts } from "./functions/products";
+import { createFirebaseCart, getFirebaseCart  } from "./functions/cart";
+import { addProductToCart, getMyLocalCart, currencyFormat } from "../utils";
+
+const productSection = document.getElementById("products");
+const categoryFilter = document.getElementById("category");
+const orderFilter = document.getElementById("order");
+
+let userLogged = undefined;
+let products = [];
+let cart = [];
+
+async function loadProducts() {
+    const firebaseProducts = await getProducts(db);
+    productSection.innerHTML = "";
+    firebaseProducts.forEach(product => {
+        renderProduct(product);
+    });
+
+    products = firebaseProducts;
+}
+
+
+
+function renderProduct(item) {
+    const product = document.createElement("a");
+    product.className = "product";
+
+    product.setAttribute("href", `./product.html?id=${item.id}`);
+
+    const coverImage = item.images ? item.images[0] : "https://cdn1.iconfinder.com/data/icons/business-company-1/500/image-512.png";
+
+    const isProductAddedToCart = cart.some((productCart) => productCart.id === item.id);
+
+    const productButtonCart = isProductAddedToCart ?
+    '<button class="product__cart" disabled>Producto añadido</button>' :
+    '<button class="product__cart">Añadir al carrito</button>';
+
+    product.innerHTML = `
+    <img src="${coverImage}" alt="" class="product__image">
+    <div class="product__info">
+        <p class="product__category">${item.category}</p> 
+        <h2 class="product__name">${item.name}</h2>
+        <h3 class="product__price">${currencyFormat(item.price)}</h3>
+        ${productButtonCart}
+    </div>
+    `;
+
+    productSection.appendChild(product);
+
+    const productCartButton = product.querySelector(".product__cart");
+
+    productCartButton.addEventListener("click", async (e) => {
+        e.preventDefault(); // evitar que al dar click en el boton, funcione el enlace del padre.
+
+        cart.push(item);
+        addProductToCart(cart);
+
+        if (userLogged) {
+            await createFirebaseCart(db, userLogged.uid, cart);
+        }
+
+        productCartButton.setAttribute("disabled", true);
+        productCartButton.innerText = "Producto añadido";
+
+    });
+}
+function filterBy(){
+    const newCategory = categoryFilter.value;
+    const newOrder = orderFilter.value;
+
+    let filteredProducts = [];
+
+    if (newCategory !== "") {
+        filteredProducts = products.filter((product) => product.category === newCategory);
+    } else {
+        filteredProducts = products;
+    }
+
+    if (newOrder === "asc") {
+        filteredProducts = filteredProducts.sort((a, b) => b.price - a.price);
+    }
+
+    if (newOrder === "desc") {
+        filteredProducts = filteredProducts.sort((a, b) => a.price - b.price);
+    }
+    
+    productSection.innerHTML = "";
+    filteredProducts.forEach(product => {
+        renderProduct(product);
+    });
+
+}
+
+categoryFilter.addEventListener("change", e => {
+    filterBy();
+});
+
+orderFilter.addEventListener("change", e => {
+    filterBy();
+});
+
+
+onAuthStateChanged(auth, async (user) => {
+    if (user) {
+      
+      userLogged = user;
+      cart = await getFirebaseCart(db, userLogged.uid);
+      // ...
+    } else {
+        cart = getMyLocalCart();
+      // User is signed out
+      // ...
+    }
+
+    loadProducts();
+
+  });
